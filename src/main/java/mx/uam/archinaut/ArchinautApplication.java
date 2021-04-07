c60c009eb16caadcdc4c81c13648ee4b769c954e
Fabián Santander <hdmsantander@gmail.com>
2021-04-06 23:09:19 -0500

    Initial commit

---
+++ src/main/java/mx/uam/archinaut/ArchinautApplication.java
@@ -0,0 +1,13 @@
+package mx.uam.archinaut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchinautApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchinautApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/uam/archinaut/ArchinautApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.uam.archinaut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchinautApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
